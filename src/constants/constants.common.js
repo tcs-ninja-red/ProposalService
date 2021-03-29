c6144f298ca0b4f570f75a4b3adf948c41199a8d
vglkannan <35820063+vglkannan@users.noreply.github.com>
2021-03-29 19:45:12 +0100

    Update PORT ENV variable name

--- src/constants/constants.common.js
+++ src/constants/constants.common.js
@@ -2,8 +2,8 @@
  * Common constants across all the environments (dev, staging, prod)
  */
 process.env.NODE_ENV = "dev";
-process.env.PORT = "44302";
+//process.env.PORT = "44302";
 module.exports = {
 	env: process.env.NODE_ENV,
-	port: process.env.PORT ||44302,
+	port: process.env.API_PORT ||44302,
 };
